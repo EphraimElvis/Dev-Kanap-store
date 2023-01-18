176cbd7243a2320d68ad28cdb75e0d0271305f82
Ephraim Elvis <ephraim.elvis@yahoo.com>
2023-01-18 03:51:41 +0000

    select ioption functionality added

--- js/product.js
+++ js/product.js
@@ -1,3 +1,9 @@
+const itemImg = document.querySelector(".item__img");
+const productName = document.querySelector("#title");
+const productPrce = document.querySelector("#price")
+const productDescription = document.querySelector("#description");
+const selectColor = document.querySelector("#colors");
+
 //get page url id
 //const pageURL = window.location.href;
 const params = new URLSearchParams(document.location.search);
@@ -7,14 +13,27 @@ let id = params.get("id");
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    //console.log(data);
     showImage(data)
   })
 
 //display item
-const showImage = (img) => {
-  const itemImg = document.querySelector('.item__img');
-  itemImg.innerHTML =`<img src=${img.imageUrl} alt=${img.altTxt}>`;
+const showImage = (pr) => {  
+  itemImg.innerHTML =`<img src=${pr.imageUrl} alt=${pr.altTxt}>`;
+  productName.innerHTML = `${pr.name}`;
+  productPrce.innerHTML = `${pr.price}`;
+  productDescription.innerHTML = `${pr.description}`;
+  createOptions(pr)
+}
+
+//create select options
+const createOptions = (arr) => {
+  arr.colors.map((color) => {
+    const createOption = document.createElement('option');
+    createOption.value = color;
+    createOption.textContent = color;
+    selectColor.appendChild(createOption);
+  })
 }
 
 
