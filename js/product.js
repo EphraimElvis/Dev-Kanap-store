a1406b4226102521c6e89df646df4fcdb78c3e19
Ephraim Elvis <ephraim.elvis@yahoo.com>
2023-01-18 03:51:41 +0000

    show product image

--- js/product.js
+++ js/product.js
@@ -1,3 +1,20 @@
-//get page url
-const pageURL = window.location.href;
-console.log(pageURL)
+//get page url id
+//const pageURL = window.location.href;
+const params = new URLSearchParams(document.location.search);
+let id = params.get("id");
+
+//get data based on product id
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    showImage(data)
+  })
+
+//display item
+const showImage = (img) => {
+  const itemImg = document.querySelector('.item__img');
+  itemImg.innerHTML =`<img src=${img.imageUrl} alt=${img.altTxt}>`;
+}
+
+
